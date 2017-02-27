ad7c992baee23fcdbd462b13f0dfba92ae7659de
Tim K. Chan <timchan@berkeley.edu>
2017-02-27 14:02:53 +0000

    app starting

---
+++ Hotei/Hotei/Hotei/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  Hotei
+//
+//  Created by Tim Kit Chan on 01/02/2017.
+//  Copyright © 2017 AppBee. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ Hotei/Hotei/Hotei/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  Hotei
+//
+//  Created by Tim Kit Chan on 01/02/2017.
+//  Copyright © 2017 AppBee. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
