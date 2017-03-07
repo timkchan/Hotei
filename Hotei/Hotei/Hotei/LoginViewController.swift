b7087e70b320545d82f6debc39db1c557fe10b79
Tim K. Chan <timchan@berkeley.edu>
2017-03-07 11:30:34 +0000

    login page added, working on logout

--- Hotei/Hotei/Hotei/LoginViewController.swift
+++ Hotei/Hotei/Hotei/LoginViewController.swift
@@ -9,35 +9,88 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+    
+    let defaults = UserDefaults.standard
 
     @IBOutlet weak var userName: UITextField!
     @IBAction func loginBtn(_ sender: UIButton) {
         
-        // TODO: - Save USER Default
+        // Unwrap and bind name
+        guard let name = userName.text, name != "" else {
+            print("Name enpty")
+            return
+        }
+
+        // Compute User ID
+        let userID = abs(name.hash)
+        
+        // Save userID to UserDefault
+        defaults.set(userID, forKey: "userID")
+        
+        // Perform Segue
+        performSegue(withIdentifier: "loginSegue", sender: sender)
     }
     
-    
+    override func viewWillAppear(_ animated: Bool) {
+
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        print("Auto Login Try")
+        performSegue(withIdentifier: "loginSegue", sender: self)
+
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // Tap away to hide keyboard.
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
+    // Tap return to hide keyboard.
+    func textFieldShouldReturn(textField: UITextField!) -> Bool // called when 'return' key pressed. return NO to ignore.
+    {
+        textField.resignFirstResponder()
+        return true;
+    }
+    
 
-    /*
+    
+
     // MARK: - Navigation
 
+    // Stop performing segue automatically
+    override func shouldPerformSegue(withIdentifier identifier: String, sender: Any?) -> Bool {
+        print("checking")
+        if identifier == "loginSegue" {
+            print("checking")
+            // If user has logged in (userIF exist in UserDefaults)
+            if let userID = defaults.object(forKey: "userID") {
+                print("userID: \(userID)")
+                return true
+            } else {
+                print("userID: not set")
+                return false
+            }
+        }
+        return true
+    }
+    
+    
     // In a storyboard-based application, you will often want to do a little preparation before navigation
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         // Get the new view controller using segue.destinationViewController.
         // Pass the selected object to the new view controller.
     }
-    */
+
 
 }
